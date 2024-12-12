d55ce106fe5483b3bac51353bc683c70dc6b119b
Devanshi Srivastava <161362258+Devanshi12srivastava@users.noreply.github.com>
2024-12-12 21:44:13 +0530

    Add files via upload

---
+++ ecommerce.js
@@ -0,0 +1,61 @@
+var Mainimg=document.getElementById("Mainimg");
+var smalli=document.getElementsByClassName("smalli");
+smalli[0].onclick=function(){
+    Mainimg.src=smalli[0].src;
+}
+smalli[1].onclick=function(){
+    Mainimg.src=smalli[1].src;
+}
+smalli[2].onclick=function(){
+    Mainimg.src=smalli[2].src;
+}
+smalli[3].onclick=function(){
+    Mainimg.src=smalli[3].src;
+}
+smalli[4].onclick=function(){
+    Mainimg.src=smalli[4].src;
+}
+
+var Mainimg=document.getElementById("Mainimg");
+var smalli=document.getElementsByClassName("smalli");
+smalli[1].onclick=function(){
+    Mainimg.src=smalli[1].src;
+}
+smalli[2].onclick=function(){
+    Mainimg.src=smalli[2].src;
+}
+smalli[3].onclick=function(){
+    Mainimg.src=smalli[3].src;
+}
+smalli[4].onclick=function(){
+    Mainimg.src=smalli[4].src;
+}
+
+var Mainimg=document.getElementById("Mainimg");
+var smalli=document.getElementsByClassName("smalli");
+smalli[2].onclick=function(){
+    Mainimg.src=smalli[2].src;
+}
+smalli[3].onclick=function(){
+    Mainimg.src=smalli[3].src;
+}
+smalli[4].onclick=function(){
+    Mainimg.src=smalli[4].src;
+}
+smalli[5].onclick=function(){
+    Mainimg.src=smalli[5].src;
+}
+var Mainimg=document.getElementById("Mainimg");
+var smalli=document.getElementsByClassName("smalli");
+smalli[3].onclick=function(){
+    Mainimg.src=smalli[3].src;
+}
+smalli[4].onclick=function(){
+    Mainimg.src=smalli[4].src;
+}
+smalli[5].onclick=function(){
+    Mainimg.src=smalli[5].src;
+}
+smalli[6].onclick=function(){
+    Mainimg.src=smalli[6].src;
+}
